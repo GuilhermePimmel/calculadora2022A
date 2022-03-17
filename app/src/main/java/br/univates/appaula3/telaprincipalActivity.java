1543dd65094272d2c3e9ad7469665decfaf77dea
GuilhermePimmel <guilherme.pimmel@universo.univates.br>
2022-03-16 21:58:16 -0300

    layout tela de login

---
+++ app/src/main/java/br/univates/appaula3/telaprincipalActivity.java
@@ -0,0 +1,14 @@
+package br.univates.appaula3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaprincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telaprincipal);
+    }
+}
